ab105863f2623ef457d0e0f2d855deacf3e4c89d
3Bmodular <3bmod@bglab.local>
2023-08-29 11:36:21 -0300

    adicionando pasta com diagramas

--- js/scrit.js
+++ js/scrit.js
@@ -1,30 +1,16 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 
+draw();
 
-
-ctx.beginPath();
-ctx.arc(700, 320, 90, 0, Math.PI * 2);
-ctx.fillStyle='red';
-ctx.fill();
-
-ctx.beginPath();
-ctx.arc(700, 320, 60, 0, Math.PI * 2);
-ctx.fillStyle='black';
-ctx.fill();
-
-
-
-ctx.beginPath();
-ctx.arc(1100, 320, 90, 0, Math.PI * 2);
-ctx.fillStyle='red';
-ctx.fill();
-
-ctx.beginPath();
-ctx.arc(1100, 320, 60, 0, Math.PI * 2);
-ctx.fillStyle='black';
-ctx.fill();
-
-
-
-
+function draw() {
+	var ctx = document.getElementById('canvas').getContext('2d');
+	var img = new Image();
+	img.onload = function(){
+		for (var i = 0; i< 1; i++){
+			for (var j = 0; j<1; j++){
+				ctx.drawImage(img,100,  20,100 , 100);
+			}
+		}
+	};
+	img.src = 'img/frente.png';
+}
+	
